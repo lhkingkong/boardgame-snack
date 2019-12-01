84e23607a615cef9312a7fa1b7943e48fca62e6b
lhkingkong <lhkingkong@gmail.com>
2019-12-01 10:55:39 -0600

    Add header menu

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { faNewspaper, faUtensils, faCalendar, faShoppingCart, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -7,6 +7,8 @@ import { faNewspaper, faUtensils, faCalendar, faShoppingCart, faMapMarkedAlt } f
   styleUrls: ['./main-nav.component.scss']
 })
 export class MainNavComponent implements OnInit {
+  @Input() isHome: boolean = false;
+
   faNewspaper = faNewspaper;
   faUtensils = faUtensils;
   faCalendar = faCalendar;
